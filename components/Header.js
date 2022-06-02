344db8d5678af5c63a29ee6d5d5c3c6da1a45a28
Miguel Blanque <miguelblanque@gmail.com>
2022-06-02 08:10:38 +0200

    creando Header

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,18 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import styles from '../styles/Header.module.css'
+
 
 const Header = () => {
   return (
-    <header>
+    <header className={styles.header}>
+    
         <div className="contenedor">
-            <div>
+            <Link href="/">
+              <Image width={400} height={100} src="/img/logo.svg" alt="Imagen Logo"/>
+            </Link>
 
-            </div>
-            
-            <nav>
+            <nav className={styles.navegacion}>
                 <Link href="/">Inicio</Link>
                 <Link href="/nosotros">Nosotros</Link>
                 <Link href="/blog">Blog</Link>
