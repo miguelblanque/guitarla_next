0e5b1671d79a3e069f1a2cd37bdc5c25aa84d00b
Miguel Blanque <miguelblanque@gmail.com>
2022-06-09 19:56:46 +0200

    implementando getStaticPaths para routing dinamico

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -11,8 +11,24 @@ const EntradaBlog = ({entrada}) => {
     <div>Desde EntradaBlog</div>
   )
 }
+//getStaticPaths para routing dinamico 
+export async function getStaticPaths(){
+    const url ='http://localhost:1337/blogs'
+    const respuesta = await fetch(url)
+    const entradas = await respuesta.json()
 
-export async function getServerSideProps({query: { id }}){
+    const paths = entradas.map(entrada => ({
+        params:{ id: entrada.id}
+    }))
+
+    
+    return {
+        paths,
+        fallback: false // true: para muchas entradas, false: se contruyen las entradas por peticion
+    }
+}
+
+export async function getStaticProps({params: { id }}){
 
     const url = `http://localhost:1337/blogs/${id}`
   const respuesta = await fetch(url)
@@ -24,5 +40,19 @@ export async function getServerSideProps({query: { id }}){
         }
     }
 }
+
+// export async function getServerSideProps({query: { id }}){
+
+//     const url = `http://localhost:1337/blogs/${id}`
+//   const respuesta = await fetch(url)
+//   const entrada = await respuesta.json()
+
+//     return {
+//         props: {
+//             entrada
+//         }
+//     }
+// }
+
 
 export default EntradaBlog
