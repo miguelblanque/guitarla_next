f50d77481d8e4ce5916a45464eb6c0bea70c583c
Miguel Blanque <miguelblanque@gmail.com>
2022-06-20 22:45:01 +0200

    LocalStorage

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import {useState} from 'react'
-import { useEffect } from 'react'
+import {useState, useEffect} from 'react'
+
 import '../styles/normalize.css'
 import '../styles/globals.css'
 
@@ -7,25 +7,34 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
 
-  const [carrito, setCarrito]= useState([])
+  const [carrito, setCarrito] = useState([])
   
+
   //obtenemos el carrito de LocalStorage
-  useEffect(()=> {
-    const carritoLS = JSON.parse(localStorage.getItem("carrito")) ?? [];
-    setCarrito(carritoLS);
+  useEffect(() => {
+   
+    const carritoLS = JSON.parse(localStorage.getItem('carrito')) ?? []; 
+    console.log('Obteniendo LS carrito: ',carritoLS)
+     setCarrito(carritoLS);
+     
   }, []);
+
   //guardamos el carrito en LocalStorage
   useEffect(()=>{
-    localStorage.setItem("carrito",JSON.stringify(carrito)) ;
-   
-  },[carrito]);
+    localStorage.setItem('carrito',JSON.stringify(carrito)) ;
+        console.log('Guardando en LS: ',carrito)
+       
+  }, [carrito]);
+
+
+
 
   const agregarCarrito = (producto) => {
     //Control producto duplicado para actualizar cantidad
-    if(carrito.some(articulo => articulo.id=== producto.id)){
-        const carritoActualizado = carrito.map( articulo => {
+    if(carrito.some(articulo => articulo.id === producto.id)){
+        const carritoActualizado = carrito.map( ( articulo) => {
           if(articulo.id === producto.id){
-            articulo.cantidad=producto.cantidad
+            articulo.cantidad=producto.cantidad;
           }
           return articulo;
         });
