d5962c0a390102ae5914071113a54f504b828aeb
Miguel Blanque <miguelblanque@gmail.com>
2022-07-24 14:17:57 +0200

    eliminar package-lock.json

--- components/Layout.js
+++ components/Layout.js
@@ -9,10 +9,7 @@ const Layout = ({children,pagina,guitarra}) => {
          <title>GuitarLA - {pagina}</title>
          <meta name="description" content="Sitio Web de venta de guitarras"/>
          
-         <link rel="preconnect" href="https://fonts.googleapis.com"/>
-         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-         <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap" rel="stylesheet"/>
-
+       
              </Head>
        
        <Header
